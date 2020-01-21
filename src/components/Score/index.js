f4badfd70435f7202243ec7a935e6a21f590622a
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-01-20 19:15:51 -0800

    added description to score component.

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 
+// Score card created within heading line
 function Score(props) {
     return <h2 className="text-canter score">Score: {props.score} || Top Score: {props.topScore}</h2>;
 }
