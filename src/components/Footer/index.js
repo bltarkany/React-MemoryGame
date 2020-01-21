2ac48e315ab9387e6cce68764bc56d73127fd9b5
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-01-20 19:12:07 -0800

    Added centering for text on footer and description of footer component.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./style.css";
 
+// footer with bootstrap styling
 function Footer() {
-    return <footer className="fixed-bottom">React Clicky Game</footer>;
+    return <footer className="fixed-bottom text-center">React Clicky Game</footer>;
 }
 
 export default Footer;
